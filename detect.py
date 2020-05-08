80c7a1b95bfd2948a151621ef50ccd69ceb710ea
jhoowy <jhoojh1@naver.com>
2020-05-08 14:50:30 +0900

    Finalize the detection model

--- detect.py
+++ detect.py
@@ -109,8 +109,6 @@ def detect(save_img=False):
                             file.write(('%g ' * 6 + '\n') % (*xyxy, cls, conf))
 
                     if save_img or view_img:  # Add bbox to image
-                        if names[int(cls)] not in  ["bottle"]:
-                            continue
                         mosaic_one_box(xyxy, im0)
                         # label = '%s %.2f' % (names[int(cls)], conf)
                         # plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
@@ -143,7 +141,7 @@ def detect(save_img=False):
     # Add audio to results
     if isinstance(vid_writer, cv2.VideoWriter):
         vid_writer.release()
-    add_audio(source, out)
+        add_audio(source, out)
 
     if save_txt or save_img:
         print('Results saved to %s' % os.getcwd() + os.sep + out)
@@ -156,13 +154,13 @@ def detect(save_img=False):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='*.cfg path')
-    parser.add_argument('--names', type=str, default='data/coco.names', help='*.names path')
+    parser.add_argument('--names', type=str, default='data/inapp_obj.names', help='*.names path')
     parser.add_argument('--weights', type=str, default='weights/yolov3-spp-ultralytics.pt', help='weights path')
     parser.add_argument('--source', type=str, default='data/samples', help='source')  # input file/folder, 0 for webcam
     parser.add_argument('--output', type=str, default='output', help='output folder')  # output folder
     parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
+    parser.add_argument('--conf-thres', type=float, default=0.1, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.4, help='IOU threshold for NMS')
     parser.add_argument('--fourcc', type=str, default='mp4v', help='output video codec (verify ffmpeg support)')
     parser.add_argument('--half', action='store_true', help='half precision FP16 inference')
     parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
