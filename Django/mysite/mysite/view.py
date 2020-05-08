ce445bd1304a7a691c944024a9f6ab84bb4faea5
D0peBayMax <zthwsygxnn@gmail.com>
2020-05-08 18:47:35 +0900

    Finish the Django(include ajax post, html/css/js on homepage)

---
+++ Django/mysite/mysite/view.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators import csrf
+ 
+def homepage(request):
+    return render(request,'homepage.html')
+
+def upload(request):
+    file = request.FILES.get('file')# Got the file, it is binary, it can be operated directly
+    with open(file.name,'wb') as f:
+        for line in file:
+            f.write(line)
+    return HttpResponse('succed')#Here is just get the file from client to server, we can do the operation here.
