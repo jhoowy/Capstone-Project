7eadebb79e1152dfca83034ca07eb5b7b9acd1f9
Heo JaeSeung <ja12304@naver.com>
2020-05-29 18:02:34 +0900

    Add authentication function on search, edit page

--- Django/mysite/mysite/view.py
+++ Django/mysite/mysite/view.py
@@ -18,20 +18,21 @@ import mimetypes
 
 vid_formats = ['.mp4', '.avi', '.mpg', '.mpeg', '.wmv']
 
- 
+
 def homepage(request):
     username = request.session.get('username')
     if username is not None:
-        label = "<a class='loginlogo' href='/logout'>Logout</a><a class='loginlogo' href='/'>Welcome Back "+username+" !   </a>"
-        return render(request,'homepage.html',{"logs":label})
+        label = "<a class='loginlogo' href='/search'>Search</a><a class='loginlogo' href='/logout'>Logout</a><a class='loginlogo' href='/'>Welcome Back " + username + " !   </a>"
+        return render(request, 'homepage.html', {"logs": label})
     else:
-        label = "<a class='loginlogo' href='/signup'>Sign Up</a><a class='loginlogo' href='/login'>Login</a>"
-        return render(request,'homepage.html',{"logs":label})
+        label = "<a class='loginlogo' href='/search'>Search</a><a class='loginlogo' href='/signup'>Sign Up</a><a class='loginlogo' href='/login'>Login</a>"
+        return render(request, 'homepage.html', {"logs": label})
+
 
 def upload(request):
-    file = request.FILES.get('file') # Got the file, it is binary, it can be operated directly
+    file = request.FILES.get('file')  # Got the file, it is binary, it can be operated directly
     formats = Path(file.name).suffix.lower()
-    
+
     if formats not in vid_formats:
         return HttpResponse('Failed to upload : Invalid extension')
 
@@ -44,7 +45,9 @@ def upload(request):
     video = Videos(video_id=video_id, file_name=file.name, user=request.user)
     video.save()
 
-    return HttpResponse('succeed. video_id : {}'.format(video_id)) #Here is just get the file from client to server, we can do the operation here.
+    return HttpResponse('succeed. video_id : {}'.format(
+        video_id))  # Here is just get the file from client to server, we can do the operation here.
+
 
 def login(request):
     if request.method == "POST":
@@ -60,22 +63,24 @@ def login(request):
     else:
         return render(request, 'login.html')
 
+
 def signup(request):
     if request.method == "POST":
         if request.POST["password1"] == request.POST["password2"]:
             user = User.objects.create_user(
-                username=request.POST["username"],password=request.POST["password1"])
+                username=request.POST["username"], password=request.POST["password1"])
             auth.login(request, user)
             return redirect("homepage")
         return render(request, "signup.html")
     return render(request, "signup.html")
+
 
 def edit_download(request, video_id):
     try:
         video_object = get_object_or_404(Videos, pk=video_id)
     except Videos.DoesNotExist:
         return HttpResponse("Video id doesn't exists.")
-    
+
     path = os.path.join(settings.MEDIA_ROOT, video_object.file_name)
     with open(path, 'rb') as f:
         mime_type, _ = mimetypes.guess_type(path)
@@ -85,39 +90,49 @@ def edit_download(request, video_id):
 
 
 def edit(request, video_id=None):
-    if video_id is None:
-        return HttpResponse("No Video")
-    
-    try:
-        video_object = get_object_or_404(Videos, pk=video_id)
-    except Videos.DoesNotExist:
-        return HttpResponse("Video id doesn't exists.")
+    username = request.session.get('username')
+    if username is not None:
+        if video_id is None:
+            return HttpResponse("No Video")
 
-    video_url = settings.MEDIA_URL + video_id
+        try:
+            video_object = get_object_or_404(Videos, pk=video_id)
+        except Videos.DoesNotExist:
+            return HttpResponse("Video id doesn't exists.")
 
-    # If client send blurring request
-    if request.POST:
-        data = json.loads(request.POST.get('data', ''))
-        box_list = []
-        for i in range(len(data)):
-            x = int(data[i]['box']['x'])
-            y = int(data[i]['box']['y'])
-            w = int(data[i]['box']['w'])
-            h = int(data[i]['box']['h'])
-            ts = 1000 * float(data[i]['time'][0])
-            te = 1000 * float(data[i]['time'][1])
-            box_list.append([x, y, w, h, ts, te])
+        video_url = settings.MEDIA_URL + video_id
 
-        save_path = os.path.join(settings.MEDIA_ROOT, video_object.file_name)
-        mosaic_video(os.path.join(settings.MEDIA_ROOT, video_object.video_id), save_path, box_list)
-        
-        return HttpResponse('')
+        # If client send blurring request
+        if request.POST:
+            data = json.loads(request.POST.get('data', ''))
+            box_list = []
+            for i in range(len(data)):
+                x = int(data[i]['box']['x'])
+                y = int(data[i]['box']['y'])
+                w = int(data[i]['box']['w'])
+                h = int(data[i]['box']['h'])
+                ts = 1000 * float(data[i]['time'][0])
+                te = 1000 * float(data[i]['time'][1])
+                box_list.append([x, y, w, h, ts, te])
 
-    return render(request, 'edit.html', {'video_url' : video_url})
+            save_path = os.path.join(settings.MEDIA_ROOT, video_object.file_name)
+            mosaic_video(os.path.join(settings.MEDIA_ROOT, video_object.video_id), save_path, box_list)
+
+            return HttpResponse('')
+
+        return render(request, 'edit.html', {'video_url': video_url})
+    else:
+        return render(request, 'login.html')
+
 
 def logout(request):
     request.session.flush()
     return redirect("homepage")
 
+
 def search(request):
-    return render(request,'search.html')
+    username = request.session.get('username')
+    if username is not None:
+        return render(request, 'search.html')
+    else:
+        return render(request, 'login.html')
