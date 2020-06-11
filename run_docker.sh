4d7fac825ffcafbbe5c30c909d9d02f1f735ed8a
jhoowy <jhoowy@naver.com>
2020-06-11 14:32:22 +0000

    Fix static file issue

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 SERVER=10.0
-GPU=0
+GPU=all
 SHMEMORY=16g
-PROJECT_PATH=~/workspace/git/Capstone-Project
+PROJECT_PATH=~/Capstone-Project
 
-nvidia-docker run -e NVIDIA_VISIBLE_DEVICES=$GPU --name $SERVER -v $PROJECT_PATH:/home/Capstone-Project --rm --shm-size $SHMEMORY -p 8000:8000 -it capstone/censoring:20.02-py3
+docker run -e NVIDIA_VISIBLE_DEVICES=$GPU --gpus all --name $SERVER -v $PROJECT_PATH:/home/Capstone-Project --rm --shm-size $SHMEMORY -p 8000:8000 -it capstone/censoring:20.02-py3
